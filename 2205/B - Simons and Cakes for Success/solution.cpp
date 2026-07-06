f9cb614537e069b02165f9a8bda33f64e1ad4f59
Manish Singh <manishsinghenggindian@gmail.com>
2026-07-07 00:19:14 +0530

    Add Simons and Cakes for Success [B] from Codeforces

---
+++ 2205/B - Simons and Cakes for Success/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans=1;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                ans*=i;
+                while(n%i==0){
+                    n/=i;
+                }
+            }
+        }
+        if(n>1){
+            ans*=n;
+        }
+        cout<<ans<<endl;
+        
+    }
+}
