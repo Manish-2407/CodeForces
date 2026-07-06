a44b82a6f23a3902de03a007d1093abd972a895d
Manish Singh <manishsinghenggindian@gmail.com>
2026-07-07 00:05:53 +0530

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long n;
+        cin>>n;
+        for(long i=1;i<n;i++){
+            cout<<i<<" ";
+        }
+        cout<<n<<endl;;
+    }
+}
