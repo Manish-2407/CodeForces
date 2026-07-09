869eddd03d6bc362ca7d45378784a07fb32dd962
Manish Singh <manishsinghenggindian@gmail.com>
2026-07-10 03:16:31 +0530

    Add Watermelon [A] from Codeforces

--- 4/A - Watermelon/solution.cpp
+++ 4/A - Watermelon/solution.cpp
@@ -4,10 +4,5 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    if(n%2==0 && n!=2){
-        cout<<"YES"<<endl;
-    }
-    else{
-        cout<<"NO"<<endl;
-    }
+    cout<<(n%2==0 && n>2 ? "YES":"NO")<<endl;
 }
