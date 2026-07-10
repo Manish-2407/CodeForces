68c895b3dcc88a3671c52fa3088ba2c352e29d1c
Manish Singh <manishsinghenggindian@gmail.com>
2026-07-11 03:32:19 +0530

    Add cAPS lOCK [A] from Codeforces

---
+++ 131/A - cAPS lOCK/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s;
+    cin>>s;
+    bool b=true;
+    for(int i=1;i<s.size();i++){
+        if(islower(s[i])){
+            b=false;
+            break;
+        }
+    }
+ 
+    if(b){
+        for(int i=0;i<s.size();i++){
+            if(islower(s[i])){
+                s[i]=toupper(s[i]);
+            }
+            else{
+                s[i]=tolower(s[i]);
+            }
+        }
+    }
+    cout<<s<<endl;
+}
