689a8563961de3edd56dceddfd79177d939ee03c
Manish Singh <manishsinghenggindian@gmail.com>
2026-08-04 20:18:14 +0530

    Add Riptide [A] from Codeforces

---
+++ 2254/A - Riptide/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a[3];
+        cin>>a[0]>>a[1]>>a[2];
+        sort(a,a+3);
+        int x=min(abs(a[2]-a[1]),abs(a[1]-a[0]));
+        cout<<x<<endl;
+    }
+}
